0f9901a333edaf8cd73a36d74d2e068e8ebbc962
Erik Burns <erik@erikburns.com>
2023-05-16 14:50:06 -0700

    refactoring expenses into separate list component, improved formatting

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,24 @@
 import React, { useState } from 'react';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 function Expenses(props) {
   const [selectedYear, setSelectedYear] = React.useState('2020');
+  const filteredExpenses = props.expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
 
   const filterExpensesHandler = (selectedYear) => {
     setSelectedYear(selectedYear);
   }
 
-  const filteredExpenses = props.expenses.filter(expense => {
-    return expense.date.getFullYear().toString() === selectedYear;
-  });
-
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter onFilterExpenses={filterExpensesHandler} selected={selectedYear} />
-        {filteredExpenses.length === 0 && <p className="expenses-empty">No expenses found.</p>}
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key = {expense.id}
-            title = {expense.title}
-            amount = {expense.amount}
-            date = {expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   )
